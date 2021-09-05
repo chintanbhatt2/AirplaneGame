8b49a4596333dc6370d48ffb838eb33f97eaf761
chintanbhatt <chintanbhatt2@gmail.com>
2021-09-04 21:57:17 -0700

    Added STL files and fixed shaders

--- AirplaneGame/Program.cs
+++ AirplaneGame/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 using OpenTK.Graphics.OpenGL4;
 using OpenTK.Mathematics;
 using OpenTK.Windowing.Desktop;
@@ -20,13 +22,29 @@ namespace AirplaneGame
             gws.UpdateFrequency = FPS;
 
             nws.Title = "FlyAround";
+            nws.Size = (800, 480);
+
+            //string stlLocation = "C:\\Users\\cb\\source\\repos\\chintanbhatt2\\AirplaneGame\\Chintan_STL\\Airo1 - Elev1-1 HorizontalStab1Move-1.stl";
+            //List<float> vertsList = readSTL(stlLocation);
+            //uint triangleCount = (uint)vertsList[0];
+            //vertsList.RemoveAt(0);
+            //float[] verts = vertsList.ToArray();
+
+            //CREATES TRIANGLE 
+            float[] verts = new float[] {
+                -0.5f, -0.5f, 0.0f,
+                 0.5f, -0.5f, 0.0f,
+                 0.0f,  0.5f, 0.0f
+            };
+
 
             GameWindow window = new GameWindow(gws, nws);
 
             ShaderProgram shaderProgram = new ShaderProgram() { id = 0 };
             window.Load += () => {
-                shaderProgram = LoadShaderProgram("..\\..\\..\\..\\shaders\\vertex_shader.glsl", "..\\..\\..\\..\\shaders\\fragment_shader.glsl");
+                shaderProgram = LoadShaderProgram("..\\..\\..\\shaders\\vertex_shader.glsl", "..\\..\\..\\shaders\\fragment_shader.glsl");
             };
+
 
             window.RenderFrame += (FrameEventArgs args) =>
             {
@@ -35,10 +53,15 @@ namespace AirplaneGame
                 GL.ClearColor(1.0f, 1.0f, 1.0f, 1.0f);
                 GL.Clear(ClearBufferMask.ColorBufferBit);
 
+                double timeValue = args.Time;
+                double greenTime = (Math.Sin(timeValue) / 2.0f) + 0.5f;
 
-
+                int vertexColorLocation = GL.GetUniformLocation(shaderProgram.id, "shifting_color");
+                GL.UseProgram(shaderProgram.id);
+                GL.Uniform4(vertexColorLocation, 0.0f, greenTime, 0.0f, 1.0f);
                 //// Creates triangle
-                float[] verts = { -0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f };
+                //float[] verts = { -0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f };
+                //float[] verts = vertsList.ToArray();
                 float[] color = { 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f };
 
                 //int vao = GL.GenVertexArray();
@@ -66,12 +89,14 @@ namespace AirplaneGame
                 GL.BindBuffer(BufferTarget.ArrayBuffer, verticies);
                 GL.BufferStorage(BufferTarget.ArrayBuffer, verts.Length * sizeof(float), verts, BufferStorageFlags.None);
                 GL.EnableVertexAttribArray(0);
-                GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0);
+                //GL.VertexAttribPointer(0, (int)triangleCount*3, VertexAttribPointerType.Float, false, 0, 0);
+                GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, true, 0, 0);
 
                 GL.BindBuffer(BufferTarget.ArrayBuffer, colors);
                 GL.BufferStorage(BufferTarget.ArrayBuffer, color.Length * sizeof(float), color, BufferStorageFlags.None);
                 GL.EnableVertexAttribArray(1);
                 GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 0, 0);
+
 
                 GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
 
@@ -135,5 +160,37 @@ namespace AirplaneGame
         {
             public int id;
         }
+        public struct Vertex
+        {
+            Vector3 Position;
+            Vector3 Normal;
+            Vector3 TexCoords;
+        };
+
+        public static List<float> readSTL(string stlFile)
+        {
+            List<float> pointList = new List<float> { };
+            BinaryReader reader = new BinaryReader(File.Open(stlFile, FileMode.Open));
+
+            reader.ReadBytes(80);
+            uint triangleCount = reader.ReadUInt32();
+            pointList.Add((float)triangleCount);
+
+            float nextFloat = 0;
+            //while((nextFloat = reader.ReadSingle()) > 0)
+            for (int j = 0; j < 1; j++)
+            {
+                reader.ReadBytes(8);
+                for (int i = 0; i  < 3*3; i++)
+                {
+                    pointList.Add(reader.ReadSingle());
+                }
+
+                reader.ReadBytes(2);
+            }
+            reader.Close();
+            return pointList;
+        }
+
     }
 }
--- AirplaneGame/shaders/fragment_shader.glsl
+++ AirplaneGame/shaders/fragment_shader.glsl
@@ -1,10 +1,13 @@
 ﻿#version 330
 
-
-in vec3 color_in;
+in vec4 color_in;
 
 out vec4 color_out;
 
+
+
 void main() {
-	color_out = vec4(color_in[0], color_in[1], color_in[2], 1.0f);
+//	color_out = vec4(color_in, 0.0f);
+	//color_out = vec4(0.0f, 1.0f, 1.0f, 1.0f);
+	color_out = color_in;
 }
