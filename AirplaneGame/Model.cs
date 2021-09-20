ba6643d52f394732ec7c75514b4dac9297f311da
Chintan Bhatt <chintanbhatt2@live.com>
2021-09-20 11:01:27 -0700

    Added basic assimp support and changed structures class

---
+++ AirplaneGame/Model.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assimp;
+using OpenTK.Mathematics;
+
+
+namespace AirplaneGame
+{
+    class Model
+    {
+        public Model(string path)
+        {
+            loadModel(path);
+        }
+        public void Draw(Shader shader)
+        {
+            for(int i = 0; i < meshes.Length; i++)
+            {
+                meshes[i].Draw(shader);
+            }
+        }
+
+
+        public Structures.Tex[] textures_loaded = { };
+        public Structures.Mesh[] meshes = {};
+        public string directory;
+        public bool gammaCorrection;
+
+        private void loadModel(string path)
+        {
+            var context = new Assimp.AssimpContext();
+            Scene aiScene = context.ImportFile(path, PostProcessSteps.Triangulate | PostProcessSteps.FlipUVs);
+
+            directory = path.Substring(0, path.LastIndexOf("/"));
+
+            processNode(aiScene.RootNode, aiScene);
+        }
+        private void processNode(Node node, Scene scene)
+        {
+            for(int i = 0; i < node.MeshCount; i++)
+            {
+                Assimp.Mesh mesh = scene.Meshes[node.MeshIndices[i]];
+                meshes.Append(processMesh(mesh, scene));
+            }
+
+            for(int i = 0; i < node.ChildCount; i++)
+            {
+                processNode(node.Children[i], scene);
+            }
+        }
+
+        private Structures.Mesh processMesh(Mesh mesh, Scene scene)
+        {
+            Structures.Vertex[] vertices = { };
+            int[] indicies = { };
+            Structures.Tex[] textures = { };
+
+            for(int i = 0; i < mesh.VertexCount; i++)
+            {
+                Structures.Vertex vertex;
+                Vector3 vector;
+
+                vector.X = mesh.Vertices[i].X;
+                vector.Y = mesh.Vertices[i].Y;
+                vector.Z = mesh.Vertices[i].Z;
+
+                vertex.Position = vector;
+
+                if (mesh.HasNormals)
+                {
+                    vector.X = mesh.Normals[i].X;
+                    vector.Y = mesh.Normals[i].Y;
+                    vector.Z = mesh.Normals[i].Z;
+                }
+
+                if (mesh.HasTextureCoords(0))
+                {
+                    Vector2 vec;
+
+                    vec.X = mesh.TextureCoordinateChannels.ElementAt(0).ElementAt(i).X;
+                    vec.Y = mesh.TextureCoordinateChannels.ElementAt(0).ElementAt(i).Y;
+                    vertex.TexCoord = vec;
+
+                    vector.X = mesh.Tangents[i].X;
+                    vector.Y = mesh.Tangents[i].Y;
+                    vector.Z = mesh.Tangents[i].Z;
+
+                    vector.X = mesh.BiTangents[i].X;
+                    vector.Y = mesh.BiTangents[i].Y;
+                    vector.Z = mesh.BiTangents[i].Z;
+                }
+                else
+                    vertex.TexCoord = new Vector2(0.0f, 0.0f);
+
+                for (int i = 0; i < mesh.FaceCount; i++)
+                {
+                    Face face = mesh.Faces[i];
+
+                    for (int j = 0; j < face.IndexCount; j++)
+                    {
+                        indicies.Append(face.Indices[j]);
+                    }
+                }
+
+                Material material = scene.Materials[mesh.MaterialIndex];
+
+                Structures.Tex[] diffuseMaps = loadMaterialTextures(material, TextureType.Diffuse, "texture_diffuse");
+                textures.Concat(diffuseMaps);
+
+                Structures.Tex[] specularMaps = loadMaterialTextures(material, TextureType.Specular, "texture_specular");
+                textures.Concat(specularMaps);
+
+                Structures.Tex[] normalMaps = loadMaterialTextures(material, TextureType.Normals, "texture_normal");
+                textures.Concat(normalMaps);
+
+                Structures.Tex[] heightMaps = loadMaterialTextures(material, TextureType.Height, "texture_height");
+                textures.Concat(heightMaps);
+
+
+                return new Structures.Mesh(vertices, indicies, textures);
+            }
+
+            return new Structures.Mesh(vertices, indicies, textures);
+        }
+
+        private Structures.Tex[] loadMaterialTextures(Material mat, TextureType type, string typeName)
+        {
+            Structures.Tex[] textures;
+            for(int i = 0; i < mat.GetMaterialTextureCount(type); i++)
+            {
+                TextureSlot str;
+                mat.GetMaterialTexture(type, i, out str);
+
+                bool skip = false;
+                for (int j = 0; j < textures_loaded.Length; j++)
+                {
+                    if(textures_loaded[j])
+                }
+            }
+        }
+
+
+    }
+}
--- AirplaneGame/Structures.cs
+++ AirplaneGame/Structures.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using OpenTK.Graphics.OpenGL4;
 using OpenTK.Mathematics;
 
@@ -10,21 +11,20 @@ namespace AirplaneGame
 {
     public class Structures
     {
-        public class Vertex
+        private const int sizeOfVertex = sizeof(float) * 8;
+        public struct Vertex
         {
-            public Vector3 Coord;
-            public Vector3 texCoord;
-            public Vertex(float x, float y, float z)
-            {
-                this.Coord = new Vector3(x, y, z);
-            }
-            public Vertex(float x, float y, float z, float u, float v, float w)
-            {
-                this.Coord = new Vector3(x, y, z);
-                this.texCoord = new Vector3(u, v, w);
-            }
-
+            public Vector3 Position, Normal; 
+            public Vector2 TexCoord;
         }
+
+        public struct Tex
+        {
+            public int id;
+            public string type;
+            public int al;
+        }
+
         public class Triangle
         {
             public List<Vertex> Verticies = new List<Vertex>();
@@ -50,8 +50,81 @@ namespace AirplaneGame
 
         public class Mesh
         {
-            public List<Triangle> Triangles = new List<Triangle>();
-            public uint triangleCount;
+            
+            public Vertex[] vertices = { };
+            public int[] indicies = { };
+            Tex[] textures = { };
+
+            public Mesh(Vertex[] vertices, int[] indicies, Tex[] textures)
+            {
+                this.vertices = vertices;
+                this.indicies = indicies;
+                this.textures = textures;
+
+                setupMesh();
+            }
+
+            private int VAO, VBO, EBO;
+            private void setupMesh()
+            {
+                VAO = GL.GenVertexArray();
+                VBO = GL.GenBuffer();
+                EBO = GL.GenBuffer();
+
+                
+
+                GL.BindVertexArray(VAO);
+                GL.BindBuffer(BufferTarget.ArrayBuffer, VBO);
+                GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeOfVertex, vertices, BufferUsageHint.StaticDraw);
+
+                GL.BindBuffer(BufferTarget.ArrayBuffer, EBO);
+                GL.BufferData(BufferTarget.ArrayBuffer, indicies.Length * sizeof(int), indicies, BufferUsageHint.StaticDraw);
+
+                //positions
+                GL.EnableVertexAttribArray(0);
+                GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, sizeOfVertex, 0);
+
+                //normals
+                GL.EnableVertexAttribArray(1);
+                GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, sizeOfVertex, sizeof(float) * 3);
+
+                //tex coords
+                GL.EnableVertexAttribArray(2);
+                GL.VertexAttribPointer(2, 2, VertexAttribPointerType.Float, false, sizeOfVertex, sizeof(float) * 6);
+
+                GL.BindVertexArray(0);
+            }
+            public void Draw(Shader shader)
+            {
+                int diffuseNo = 1;
+                int specularNo = 1;
+
+                for(int i = 0; i < textures.Length; i++)
+                {
+                    GL.ActiveTexture(TextureUnit.Texture0 + 1);
+
+                    string num = "";
+                    string name = textures[i].type;
+                    if (name == "teture_diffuse")
+                    {
+                        num = diffuseNo++.ToString();
+                    }
+                    else if (name == "texture_specular")
+                    {
+                        num = specularNo++.ToString();
+                    }
+
+                    shader.SetFloat(("material." + name + num), i);
+                    GL.BindTexture(TextureTarget.Texture2D, textures[i].id);
+                }
+
+                GL.ActiveTexture(TextureUnit.Texture0);
+
+                GL.BindVertexArray(VAO);
+                GL.DrawElements(BeginMode.Triangles, indicies.Length, DrawElementsType.UnsignedInt, 0);
+                GL.BindVertexArray(0);
+  
+            }
         }
     }
 }
